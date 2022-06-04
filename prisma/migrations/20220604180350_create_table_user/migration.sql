78c705fc271df7887fab602c876a0cc0023e43ed
davi-souza2001 <davisouza2001dv@gmail.com>
2022-06-04 15:04:22 -0300

    change to postgres

--- prisma/migrations/20220604180350_create_table_user/migration.sql
+++ prisma/migrations/20220604180350_create_table_user/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "nickname" TEXT NOT NULL,
     "email" TEXT NOT NULL,
@@ -15,8 +15,16 @@ CREATE TABLE "users" (
     "youtube" TEXT NOT NULL,
     "instagram" TEXT NOT NULL,
     "avatar" TEXT NOT NULL,
-    "gas" INTEGER NOT NULL
+    "gas" INTEGER NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "users_nickname_key" ON "users"("nickname");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_github_key" ON "users"("github");
